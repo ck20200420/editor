dbf91d22373ce950da31eb463e5703e5b53ebaae
Bryan <ck20200420@gmail.com>
2020-09-17 17:20:41 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <textarea v-model="context"></textarea>
+    <button @click="getText">GET</button>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    script: [
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js' },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/tinymce/5.0.5/tinymce.min.js' }
+    ]
+  },
+  data() {
+    return {
+      context: 'test'
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      tinymce.init({
+        selector: "textarea",
+        plugins: "lists advlist autolink autoresize charmap code emoticons hr image insertdatetime link media paste preview searchreplace table textpattern toc visualblocks visualchars wordcount quickbars",
+        toolbar: "fontsizeselect | bold italic underline strikethrough backcolor | subscript superscript | numlist bullist | alignleft aligncenter alignright alignjustify | outdent indent | paste searchreplace | toc link image media charmap insertdatetime emoticons hr | table tabledelete | tableprops tablerowprops tablecellprops | tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol | removeformat",
+        insertdatetime_element: true,
+        media_scripts: [
+        {filter: 'platform.twitter.com'},
+        {filter: 's.imgur.com'},
+        {filter: 'instagram.com'},
+        {filter: 'https://platform.twitter.com/widgets.js'},
+      ],
+        browser_spellcheck: true,
+        contextmenu: false,
+      });
+    }, 100);
+  },
+  methods: {
+    getText() {
+      var conconet = tinymce.activeEditor.getContent()
+      console.log(conconet)
+    }
+  }
+}
+</script>

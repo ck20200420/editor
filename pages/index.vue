6ee8cd00feb1361787660001e9a1dbe46843c7e6
Bryan <ck20200420@gmail.com>
2020-09-22 17:49:30 +0800

    no message

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,51 @@
 <template>
   <div class="container">
-    <textarea v-model="context"></textarea>
-    <button @click="getText">GET</button>
+    <editor
+        v-model="content"
+        api-key="csbjkrxgrzjz43jwb0eem0uxkpwdqnrmfgjpkul70yxcp2tg"
+        :init="init"
+     />
+     <input name="image" type="file" id="upload" class="hidden" onchange="">
   </div>
 </template>
 
 <script>
+import $ from 'jquery'
+import Editor from '@tinymce/tinymce-vue'
 export default {
-  head: {
-    script: [
-      { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js' },
-      { src: 'https://cdnjs.cloudflare.com/ajax/libs/tinymce/5.0.5/tinymce.min.js' }
-    ]
-  },
   data() {
     return {
-      context: 'test'
+      content: 'test',
+      init: {
+        language: 'zh_TW',
+        menubar: false,
+        plugins: [
+          'lists advlist autolink autoresize charmap emoticons image insertdatetime link paste textpattern toc visualblocks',
+        ],
+        toolbar: 'fontsizeselect forecolor backcolor bold italic underline strikethrough | numlist bullist | aligncenter alignleft alignright | link image emoticons',
+        paste_data_images: false,
+        image_advtab: true,
+        file_picker_callback: function(callback, value, meta) {
+          if (meta.filetype == 'image') {
+            $('#upload').trigger('click');
+            $('#upload').on('change', function() {
+              var file = this.files[0];
+              var reader = new FileReader();
+              reader.onload = function(e) {
+                console.log(e)
+                callback(e.target.result, {
+                  alt: ''
+                });
+              };
+              reader.readAsDataURL(file);
+            });
+          }
+        }
+      }
     }
   },
-  mounted() {
-    setTimeout(() => {
-      tinymce.init({
-        selector: "textarea",
-        plugins: "lists advlist autolink autoresize charmap code emoticons hr image insertdatetime link media paste preview searchreplace table textpattern toc visualblocks visualchars wordcount quickbars",
-        toolbar: "fontsizeselect | bold italic underline strikethrough backcolor | subscript superscript | numlist bullist | alignleft aligncenter alignright alignjustify | outdent indent | paste searchreplace | toc link image media charmap insertdatetime emoticons hr | table tabledelete | tableprops tablerowprops tablecellprops | tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol | removeformat",
-        insertdatetime_element: true,
-        media_scripts: [
-        {filter: 'platform.twitter.com'},
-        {filter: 's.imgur.com'},
-        {filter: 'instagram.com'},
-        {filter: 'https://platform.twitter.com/widgets.js'},
-      ],
-        browser_spellcheck: true,
-        contextmenu: false,
-      });
-    }, 100);
-  },
-  methods: {
-    getText() {
-      var conconet = tinymce.activeEditor.getContent()
-      console.log(conconet)
-    }
+  components: {
+    'editor': Editor
   }
 }
 </script>
